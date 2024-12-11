7e431609f48244b9eb1593764e060093bf1eccc5
frederikbf1 <frfa@itu.dk>
2024-12-11 13:04:40 +0100

    Updated

--- dataloading/huggingface.py
+++ dataloading/huggingface.py
@@ -2,10 +2,14 @@ import transformers
 import torch
 from huggingface_hub import login
 import csv
+import os
+from dotenv import load_dotenv
 
-path = ""
+load_dotenv()
 
-login(token="")
+path = "../data/"
+
+login(token=os.getenv('HF_TOKEN'))
 
 model_id = "meta-llama/Llama-3.1-8B-Instruct"
 
@@ -13,7 +17,7 @@ pipeline = transformers.pipeline(
     "text-generation",
     model=model_id,
     model_kwargs={"torch_dtype": torch.bfloat16},
-    #device_map="auto",
+    device_map="auto",
 )
 
 data_path = path + "reddit.csv"
@@ -25,9 +29,15 @@ with open(data_path, 'r', encoding='utf-8') as csv_file:
             break
         cnt+=1
         message = row['text']
+        print("Message:")
+        print(message)
+
+        message = "Hej, jeg hedder Frederik."
         messages = [
-            {"role": "system", "content": "Please, rewrite the message that you are fed from the user such that it has the same meaning."},
-            {"role": "user", "content": message},
+            #{"role": "system", "content": "You are a Danish re-writing agent. You have to re-write the input to have the same meaning"},
+            {"role": "system", "content": "You are a pirate"},
+            #{"role": "user", "content": "Please, re-write the following text: \n\n" + message},
+            {"role": "user", "content": "Hello, how are you?"}
         ]
         terminators = [
             pipeline.tokenizer.eos_token_id,
@@ -35,10 +45,11 @@ with open(data_path, 'r', encoding='utf-8') as csv_file:
         ]
         outputs = pipeline(
             messages,
-            max_new_tokens=1000,
+            max_new_tokens=50,
             eos_token_id=terminators,
             do_sample=True,
             temperature=0.6,
             top_p=0.9,
+            pad_token_id = pipeline.tokenizer.eos_token_id
         )
         print(outputs[0]["generated_text"][-1])#, flush=True)
