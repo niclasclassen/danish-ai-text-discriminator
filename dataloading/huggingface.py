46f7894def4248cde20a38ff5301ef6f4bc25449
Niclas <69795173+niclasclassen@users.noreply.github.com>
2024-12-11 15:19:27 +0100

    update hf model

--- dataloading/huggingface.py
+++ dataloading/huggingface.py
@@ -9,9 +9,9 @@ load_dotenv()
 
 path = "../data/"
 
-login(token=os.getenv('HF_TOKEN'))
+login(token=os.getenv("HF_TOKEN"))
 
-model_id = "meta-llama/Llama-3.1-8B-Instruct"
+model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
 
 pipeline = transformers.pipeline(
     "text-generation",
@@ -20,36 +20,47 @@ pipeline = transformers.pipeline(
     device_map="auto",
 )
 
-data_path = path + "reddit.csv"
-with open(data_path, 'r', encoding='utf-8') as csv_file:
+# data_path = path + "reddit.csv"
+data_path = "/home/niclc/nlp_project/reddit_cleaned.csv"
+with open(data_path, "r", encoding="utf-8") as csv_file:
     csv_reader = csv.DictReader(csv_file)
     cnt = 0
     for row in csv_reader:
         if cnt == 5:
             break
-        cnt+=1
-        message = row['text']
+        cnt += 1
+        message = row["text"]
         print("Message:")
         print(message)
 
-        message = "Hej, jeg hedder Frederik."
+        # message = "Hej, jeg hedder Frederik."
         messages = [
-            #{"role": "system", "content": "You are a Danish re-writing agent. You have to re-write the input to have the same meaning"},
-            {"role": "system", "content": "You are a pirate"},
-            #{"role": "user", "content": "Please, re-write the following text: \n\n" + message},
-            {"role": "user", "content": "Hello, how are you?"}
+            {
+                "role": "system",
+                "content": "You are a Danish re-writing agent. You have to re-write the input to have the same meaning.",
+            },
+            # {"role": "system", "content": "You are a pirate"},
+            {
+                "role": "user",
+                "content": "Please, re-write the following text in danish for a reddit post and keep the links: \n\n"
+                + message,
+            },
+            # {"role": "user", "content": "Hello, how are you?"}
         ]
         terminators = [
             pipeline.tokenizer.eos_token_id,
-            pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+            pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>"),
         ]
         outputs = pipeline(
             messages,
-            max_new_tokens=50,
+            max_new_tokens=150,
             eos_token_id=terminators,
             do_sample=True,
             temperature=0.6,
             top_p=0.9,
-            pad_token_id = pipeline.tokenizer.eos_token_id
+            pad_token_id=pipeline.tokenizer.eos_token_id,
         )
-        print(outputs[0]["generated_text"][-1])#, flush=True)
+        print("Origianl text", flush=True)
+        print(message, flush=True)
+        print("---------------------", flush=True)
+        print(outputs[0]["generated_text"][-1], flush=True)
